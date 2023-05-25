90092475137f8521f187bb646494cf8d26a15b1c
FabioCostaa <fabio.glz4@gmail.com>
2023-05-25 19:12:43 -0300

    first commit

---
+++ src/data/questions_complete.js
@@ -0,0 +1,78 @@
+const data = [
+  {
+    category: "Disney",
+    questions: [
+      {
+        question: "Qual filme da Disney traz uma jovem princesa que tem uma madrasta malvada e é ajudada por sete anões?",
+        options: ["Branca de Neve", "A Bela e a Fera", "A Bela Adormecida", "Cinderela"],
+        answer: "Branca de Neve",
+        tip: "A madastra possui um espelho que fala",
+      },
+      {
+        question: "Qual filme da Disney se passa em um reino de gelo e traz as aventuras das irmãs Elsa e Anna?",
+        options: ["Valente", "Enrolados", "Mulan", "Frozen"],
+        answer: "Frozen",
+        tip: "Tem um boneco de neve que se chama Olaf",
+      },
+      {
+        question: "Qual filme da Disney conta a história de um brinquedo cowboy que se torna o líder de um grupo de brinquedos ?",
+        options: ["Procurando Nemo", "Wall-E", "Toy Story", "Up - Altas Aventuras"],
+        answer: "Toy Story",
+        tip: "Tem um brinquedo que é 'astronauta' ",
+      },
+      {
+        question: "Qual filme da Disney conta a história de uma menina que entra em um mundo de fantasia através de um guarda-roupa mágico ?",
+        options: ["Alice no País das Maravilhas", "A Bela Adormecida", "As Crônicas de Nárnia", "Peter Pan"],
+        answer: "As Crônicas de Nárnia",
+        tip: "Neste filme tem um leão falante que se chama Aslan",
+      },
+      {
+        question: "Em Rei Leão, qual é o nome dos amigos de Simba ?",
+        options: ["Timão e Pumba", "Porco e Suricano", "Pumbar e Timo", "Não tem nome"],
+        answer: "Timão e Pumba",
+        tip: "As iniciais de seus nome é T e P",
+      },
+    ],
+  },
+  {
+    category: "Cartoon Network",
+    questions: [
+      {
+        question: "Qual filme da Cartoon Network traz um grupo de jovens super-heróis que protege a cidade de Townsville?",
+        options: ["O Poderoso Cachorrão", "As Meninas Superpoderosas", "O Laboratório de Dexter", "A Vaca e o Frango"],
+        answer: "As Meninas Superpoderosas",
+        tip: "São 3 irmãs",
+      },
+      {
+        question: "Qual filme da Cartoon Network traz um garoto de 11 anos que possui um relógio que lhe dá a habilidade de se transformar em diferentes alienígenas?",
+        options: ["A Mansão Foster para Amigos Imaginários", "Hora De Aventura", "Ben 10", "Os Jovens Titãs "],
+        answer: "Ben 10",
+        tip: "Ele está sempre acompanhado de seu avô e sua prima.",
+      },
+      {
+        question: "Qual o desenho que tem um pássaros e um guaxinim que vivem em um parque?",
+        options: ["O Incrível Mundo de Gumball", "Titio Avô", "Hora De Aventura", "while"],
+        answer: "while",
+        tip: "Seus nome são Mordecai e Rigby",
+      },
+      {
+        question: "Qual desenho é formado por um garoto e um cachorro que pode se esticar ?",
+        options: [
+          "Hora De Aventura",
+          "Steven Universo",
+          "Apenas um Show",
+          "Clarêncio, o Otimista"],
+        answer: "Hora De Aventura",
+        tip: "Seus nomes são Finn e Jake",
+      },
+      {
+        question: "Qual desenho do Cartoon Network traz um grupo de super-heróis formado por cinco jovens com habilidades especiais ?",
+        options: ["A Mansão Foster para Amigos Imaginários", "Steven Universo", "x = 10;", "Ben 10"],
+        answer: "Jovens Titãs em Ação!",
+        tip: "Eles lutam contra o crime e protegem Jump City.",
+      },
+    ],
+  },
+];
+
+export default data;
